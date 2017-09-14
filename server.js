c0a09a8ce6b946df1856de128a2e34d8cd7852ac
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-09-14 14:45:27 -0700

    Add scaffold.

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(`${__dirname}/build`));
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}.`);
+});
