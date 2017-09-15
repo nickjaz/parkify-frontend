fb97ce027fde47a13cfe84ee04385746124be52c
Allan Liebold <allan.liebold@gmail.com>
2017-09-15 15:36:15 -0700

    creates header component

---
+++ src/components/header/index.js
@@ -0,0 +1,14 @@
+import './_header.scss';
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <section className='header'>
+
+      </section>
+    )
+  }
+}
+
+export default Header;
