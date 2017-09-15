bf270df9036b3bc9a297b610cf729eb605765d45
sharmarke <smarky292@gmail.com>
2017-09-15 13:26:52 -0700

    adds navbar to app component

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 
+import Navbar from '../navbar';
+
+
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
         <div className='parkify'>
+          <Route exact path='*' component={Navbar} />
+
             <Content>
             </Content>
             <Footer />
