a05aa4a33a9784611b522142fb9a4e19355c5562
sharmarke <smarky292@gmail.com>
2017-09-15 15:40:26 -0700

    added semicolons

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,14 +1,14 @@
 import './_navbar.scss';
-import React from 'react'
-import {connect} from 'react-redux'
-import {Redirect, Link} from 'react-router-dom'
+import React from 'react';
+import {connect} from 'react-redux';
+import {Redirect, Link} from 'react-router-dom';
 
-import Icon from '../icon-component'
-import Avatar from '../avatar'
-import {tokenSet} from '../../action/auth-actions.js'
-import * as util from '../../lib/utilities.js'
-import * as authActions from '../../action/auth-actions.js'
-import {profileFetchRequest} from '../../action/profile-actions.js'
+import Icon from '../icon-component';
+import Avatar from '../avatar';
+import {tokenSet} from '../../action/auth-actions.js';
+import * as util from '../../lib/utilities.js';
+import * as authActions from '../../action/auth-actions.js';
+import {profileFetchRequest} from '../../action/profile-actions.js';
 
 let NavLink = (props) => (
   <li className={util.classToggler({selected: props.url === `/${props.route}` })} >
@@ -20,36 +20,36 @@ let NavLink = (props) => (
 
 class Navbar extends React.Component {
   constructor(props){
-    super(props)
-    this.validateRoute = this.validateRoute.bind(this)
-    this.handleLogout = this.handleLogout.bind(this)
+    super(props);
+    this.validateRoute = this.validateRoute.bind(this);
+    this.handleLogout = this.handleLogout.bind(this);
   }
 
   componentDidMount(){
-    this.validateRoute(this.props)
+    this.validateRoute(this.props);
   }
 
   validateRoute(props){
-    let {match, history} = props
-    let token = util.readCookie('Parkify-Token')
+    let {match, history} = props;
+    let token = util.readCookie('Parkify-Token');
 
     if(!token){
-      return history.replace('/welcome/signup')
+      return history.replace('/welcome/signup');
     }
 
-    this.props.tokenSet(token)
-    this.props.profileFetch()
+    this.props.tokenSet(token);
+    this.props.profileFetch();
     .catch(() => {
-      console.log('PROFILE FETCH ERROR: user does not have a profile')
+      console.log('PROFILE FETCH ERROR: user does not have a profile');
       if(!match.url.startsWith('/settings')){
-        return history.replace('/settings')
+        return history.replace('/settings');
       }
     })
   }
 
   handleLogout(){
-    this.props.logout()
-    this.props.history.push('/welcome/login')
+    this.props.logout();
+    this.props.history.push('/welcome/login');
   }
 
   render(){
@@ -90,12 +90,12 @@ class Navbar extends React.Component {
 let mapStateToProps = (state) => ({
   loggedIn: !!state.auth,
   profile: state.profile,
-})
+});
 
 let mapDispatchToProps = (dispatch) => ({
   logout: () => dispatch(authActions.logout()),
   tokenSet: (token) => dispatch(tokenSet(token)),
   profileFetch: () => dispatch(profileFetchRequest()),
-})
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Navbar)
