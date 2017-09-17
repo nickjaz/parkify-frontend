948e184abc7d0b0c1e3a5c24475ed643201b184a
ohjonah <ohjonah@gmail.com>
2017-09-16 17:00:19 -0700

    Adds Lot Actions

---
+++ src/action/host-lot-actions.js
@@ -0,0 +1,67 @@
+import superagent from 'superagent';
+
+export const fetchLots = (lots) => ({
+  type: 'DISPLAY_LOTS',
+  payload: lots
+});
+
+export const createLot = (lot) => ({
+  type: 'CREATE_LOT',
+  payload: lot
+});
+
+export const updateLot = (lot) => ({
+  type:'UPDATE_LOT',
+  payload: lot
+});
+
+export const deleteLot = (lot) => ({
+  type: 'DELETE_LOT',
+  payload: lot
+});
+
+export const fetchLotsRequest = () => (dispatch, getState) => {
+  let {auth} = getState();
+
+  return superagent.get(`${__API_URL__}/api/lots`)
+    .set('Authorization', `Bearer ${auth}`)
+    .then(response => {
+      dispatch(fetchLots(response.body));
+      return response;
+    });
+};
+
+export const createLotRequest = (lot) => (dispatch, getState) => {
+  let {auth} = getState();
+
+  return superagent.post(`${__API_URL__}/api/lot`)
+    .set('Authorization', `Bearer ${auth}`)
+    .send(lot)
+    .then(response => {
+      dispatch(createLot(response.body));
+      return response;
+    });
+};
+
+export const updateLotRequest = (lot) => (dispatch, getState) => {
+  let {auth} = getState();
+
+  return superagent.put(`${__API_URL__}/api/lot/${lot._id}`)
+    .set('Authorization', `Bearer ${auth}`)
+    .send(lot)
+    .then(response => {
+      dispatch(updateLot);
+      return response;
+    });
+};
+
+export const deleteLotRequest = (lot) => (dispatch, getState) => {
+  let {auth} = getState();
+
+  return superagent.delete(`${__API_URL__}/lot/${photo._id}`)
+    .set('Authorization', `Bearer ${auth}`)
+    .then(response => {
+      dispatch(deleteLotRequest(lot));
+      return response;
+    });
+};
