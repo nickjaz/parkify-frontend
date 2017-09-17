19433f00b950d600fed37ec8272a4f480e3fbcf3
ohjonah <ohjonah@gmail.com>
2017-09-16 18:27:30 -0700

    Add Host Lots Reducers need to finish

---
+++ src/reducers/host-lot-reducers.js
@@ -0,0 +1,14 @@
+export default (state=[], action) => {
+  let {type, payload} = action;
+
+  switch(type) {
+    case 'CREATE_LOT':
+      return
+    case 'UPDATE_LOT':
+      return
+    case 'CREATE_':
+      return
+    case 'DELETE_LOT':
+      return
+  }
+}
