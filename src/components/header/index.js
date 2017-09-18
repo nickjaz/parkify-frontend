d7a1ff034f9eb8be8a3ce5859e1cc49ab58b1118
Allan Liebold <allan.liebold@gmail.com>
2017-09-18 13:59:26 -0700

    adds navbar to header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,7 +1,7 @@
 import './_header.scss';
 import React from 'react';
 import Modal from '../modal';
-//import Navbar from '../navbar';
+import Navbar from '../navbar';
 
 class Header extends React.Component {
   constructor(props) {
@@ -20,23 +20,16 @@ class Header extends React.Component {
   }
 
   render() {
-    let nav;
-    if(this.state.navOpen) {
-      nav =
-        <Modal>
-          <ul>
-            <li>Home</li>
-            <li>User</li>
-            <li>Other</li>
-          </ul>
-        </Modal>
-    } else {
-      nav = undefined;
-    }
+    let nav = this.state.navOpen
+    ? <Modal>
+        <Navbar />
+      </Modal>
+    : undefined;
+
     return (
       <section className='header'>
-        <button onClick={this.toggleNav}>Nav</button>
-        <div className='logo'>Logo</div>
+        <button onClick={this.toggleNav}>V</button>
+        <div className='logo'>Parkify</div>
         <div className='nav'>{nav}</div>
       </section>
     )
