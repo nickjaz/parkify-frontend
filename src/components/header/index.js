72ef519c01a4a5f9667b791f91fa2a43aab9e046
Allan Liebold <allan.liebold@gmail.com>
2017-09-18 14:05:44 -0700

    fixes indentation and adds sign out placeholder

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -21,18 +21,19 @@ class Header extends React.Component {
 
   render() {
     let nav = this.state.navOpen
-    ? <Modal>
+      ? <Modal>
         <Navbar />
       </Modal>
-    : undefined;
+      : undefined;
 
     return (
       <section className='header'>
         <button onClick={this.toggleNav}>V</button>
         <div className='logo'>Parkify</div>
         <div className='nav'>{nav}</div>
+        <div className='signout'>Sign Out</div>
       </section>
-    )
+    );
   }
 }
 
