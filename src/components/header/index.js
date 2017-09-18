e3337d401e2b2d442d6e9ea0a47fef6a97660428
Allan Liebold <allan.liebold@gmail.com>
2017-09-18 11:17:18 -0700

    creates toggle button for nav

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,10 +1,11 @@
 import './_header.scss';
 import React from 'react';
+import Modal from '../modal';
+//import Navbar from '../navbar';
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
-
     this.toggleNav = this.toggleNav.bind(this);
     this.state = {
       navOpen: false
@@ -12,18 +13,31 @@ class Header extends React.Component {
   }
 
   toggleNav() {
-    this.navOpen
-    ? this.setState({navOpen: false})
-    : this.setState({navOpen: true });
+    let navState = !this.state.navOpen;
+    this.setState({
+      navOpen: navState
+    });
   }
 
   render() {
+    let nav;
+    if(this.state.navOpen) {
+      nav =
+        <Modal>
+          <ul>
+            <li>Home</li>
+            <li>User</li>
+            <li>Other</li>
+          </ul>
+        </Modal>
+    } else {
+      nav = undefined;
+    }
     return (
       <section className='header'>
-        <button onClick={this.state.toggleNav}>Nav Icon</button>
-        {
-          //Logo go here
-        }
+        <button onClick={this.toggleNav}>Nav</button>
+        <div className='logo'>Logo</div>
+        <div className='nav'>{nav}</div>
       </section>
     )
   }
