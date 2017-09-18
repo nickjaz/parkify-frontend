4527fb617a1855f52ffb053b414c758e7713750c
Allan Liebold <allan.liebold@gmail.com>
2017-09-18 15:11:40 -0700

    creates settings-container component

---
+++ src/components/settings-container/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class SettingsContainer extends React.Component {
+  render() {
+    return (
+      <section>
+        <div>{ProfileForm}</div>
+        <div>{CarForm}</div>
+      </section>
+    );
+  }
+}
+
+export default SettingsContainer;
