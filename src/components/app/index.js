010b4cd9cf018881c2d95146b6648dd553eb8360
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-09-18 22:06:19 -0700

    Redirect if user exists.

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -8,25 +8,48 @@ import SettingsContainer from '../settings-container';
 import LandingContainer from '../landing-container';
 import {setToken} from '../../actions/auth-actions.js';
 import {fetchProfileRequest} from '../../actions/profile-actions.js';
+import * as util from '../../lib/utilities.js';
 
 import Navbar from '../navbar';
 
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.validateRoute = this.validateRoute.bind(this);
+  }
+
+  componentDidMount(){
+    this.validateRoute(this.props);
+  }
+
+  validateRoute(props){
+    let {match, history} = props;
+    let token = util.readCookie('X-Parkify-Token');
+
+    if (!token){
+      return;
+    }
+
+    this.props.setToken(token);
+    this.props.fetchProfile()
+    .then(() => history.replace('/search'))
+    .catch(() => {
+      console.log('PROFILE FETCH ERROR: user does not have a userProfile');
+
+      if(!match.url.startsWith('/settings')) {
+        history.replace('/settings');
+      }
+    });
+  }
+
   render() {
     return (
       <BrowserRouter>
         <div className='parkify'>
           <main>
-            <Route exact path='/' render={() => (
-              !this.token ? <Redirect to='/welcome/login' />
-                : this.props.profile ? (
-                  <Redirect to='/search' />
-                ) : (
-                  <Redirect to='/settings' />
-                )
-            )} /> 
-            <Route exact path='/search' component={Navbar} />
+            <Route exact path='*' component={Navbar} />
+            <Redirect from='/' to='/welcome/login' /> 
             <Route exact path='/welcome/:auth' component={LandingContainer} />
             <Route exact path='/search' component={Search} />
             <Route exact path='/settings' component={SettingsContainer} />
@@ -39,7 +62,7 @@ class App extends React.Component {
 
 let mapStateToProps = (state) => ({
   token: state.auth,
-  profile: state.profile,
+  profile: state.profile
 });
 
 let mapDispatchToProps = (dispatch) => ({
@@ -48,7 +71,10 @@ let mapDispatchToProps = (dispatch) => ({
 });
 
 App.propTypes = {
-  profile: PropTypes.object
+  profile: PropTypes.object,
+  token: PropTypes.string,
+  setToken: PropTypes.func,
+  fetchProfile: PropTypes.func
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
