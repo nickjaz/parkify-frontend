ef40e3022d2f0c8ed560bb2f23403b27c2f726be
Allan Liebold <allan.liebold@gmail.com>
2017-09-19 10:32:13 -0700

    changes main to div in modal component

--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,17 +1,23 @@
 import './_modal.scss';
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Modal extends React.Component {
   render() {
     return (
       <section className='modal'>
         <button onClick={this.props.close}>X</button>
-        <main>
+        <div className='modal-contents'>
           {this.props.children}
-        </main>
+        </div>
       </section>
-    )
+    );
   }
 }
+
+Modal.propTypes = {
+  children: PropTypes.array,
+  close: PropTypes.func
+};
 
 export default Modal;
