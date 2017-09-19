234be8058d026fca6103f2c96aae9bbf89a2354b
sharmarke <smarky292@gmail.com>
2017-09-19 10:38:24 -0700

    changes made in car form

--- src/components/car-form/index.js
+++ src/components/car-form/index.js
@@ -4,64 +4,80 @@ import * as util from '../../lib/utilities.js';
 class CarForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state {
-      make: '';
-      model: '';
-      color: '';
-      licensePlate: '';
+    this.state = {
+      make: '',
+      model: '',
+      color: '',
+      licensePlate: ''
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(e) {
-  let {name, value,} = e.target;
-  this.setState({
-    [name]: value
-  })
+    let {name, value, type} = e.target;
+
+    if(type === 'number') {
+      try {
+        this.setState({
+          [name]: parseInt(value)
+        })
+      } catch(err) {
+        console.log(err);
+      }
+    } else {
+      this.setState({
+        [name]: value
+      })
+    }
   }
-}
 
-handleSubmit(e) {
-  e.preventDefault();
-  this.props.handleSubmit(this.state);
-}
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.handleSubmit(this.state);
+  }
 
-render() {
-  return (
-    <form className='car-form'
-      onSubmit={this.handleSubmit} />
-      <input
-        name='make'
-        type='text'
-        placeholder='Car Make'
-        value={this.state.make}
-        onChange={this.handleChange}
-      />
 
-      <input
-        name='model'
-        type='number'
-        placeholder='Car Model'
-        value={this.state.model}
-        onChange={this.handleChange}
-      />
-      <input
-        name='color'
-        type='text'
-        placeholder='Car Color'
-        value={this.state.color}
-        onChange={this.handleChange}
-      />
 
-      <input
-        name='licensePlate'
-        type='number'
-        placeholder='License Number'
-        value={this.state.licensePlate}
-        onChange={this.handleChange}
-      />
-    </form>
-  )
+  render() {
+    return (
+      <div>
+        <form className='car-form'
+          onSubmit={this.handleSubmit}>
+          <input
+            name='make'
+            type='text'
+            placeholder='Car Make'
+            value={this.state.make}
+            onChange={this.handleChange}
+          />
+
+          <input
+            name='model'
+            type='text'
+            placeholder='Car Model'
+            value={this.state.model}
+            onChange={this.handleChange}
+          />
+          <input
+            name='color'
+            type='text'
+            placeholder='Car Color'
+            value={this.state.color}
+            onChange={this.handleChange}
+          />
+
+          <input
+            name='licensePlate'
+            type='number'
+            placeholder='License Number'
+            value={this.state.licensePlate}
+            onChange={this.handleChange}
+          />
+        </form>
+      </div>
+    )
+  }
 }
 export default CarForm;
