1e46e6391131def04f433a7ed13e2534172f8291
Nick Asmussen <nick.asmussen1@gmail.com>
2017-09-21 13:07:21 -0700

    finishes spot reserve form

--- src/components/spot-form/index.js
+++ src/components/spot-form/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
-import {updateSpotRequest} from '../../action/spot-actions.js';
+import {createSpotRequest} from '../../action/spot-actions.js';
 
 class SpotForm extends React.Component {
   constructor(props) {
     super(props);
 
-    let spotAvailable = this.props.lot.spots.filter(spot => spot.reserved === false).pop();
+    // let spotAvailable = this.props.lot.spots.filter(spot => spot.reserved === false).pop();
+
+    var now = new Date();
+
+    let year = now.getFullYear();
+    let month = now.getMonth().toString().length === 1 ? '0' + (now.getMonth() + 1).toString() : now.getMonth() + 1;
+    let date = now.getDate().toString().length === 1 ? '0' + (now.getDate()).toString() : now.getDate();
+    let hours = now.getHours().toString().length === 1 ? '0' + now.getHours().toString() : now.getHours();
+    let minutes = now.getMinutes().toString().length === 1 ? '0' + now.getMinutes().toString() : now.getMinutes();
+
+    let d = year + '-' + month + '-' + date + 'T' + hours + ':' + minutes;
 
     this.state = {
-      spot: spotAvailable
+      startTime: d,
+      endTime: d
     };
 
     this.handleChange = this.handleChange.bind(this);
@@ -25,11 +36,11 @@ class SpotForm extends React.Component {
 
   handleSubmit(e) {
     e.preventDefault();
-    let {spot} = this.state;
+    let spot = {...this.state};
     spot.startTime = new Date(Date.parse(spot.startTime + ':00'));
     spot.endTime = new Date(Date.parse(spot.endTime + ':00'));
-    spot.reserved = true;
-    this.props.updateSpot(this.state);
+    // spot.reserved = true;
+    this.props.createSpot(spot);
   }
 
   render() {
@@ -62,13 +73,13 @@ const mapStateToProps = (state) => ({
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    updateSpot: (spot) => dispatch(updateSpotRequest(spot))
+    createSpot: (spot) => dispatch(createSpotRequest(spot))
   };
 };
 
 SpotForm.propTypes = {
   spot: PropTypes.object,
-  updateSpot: PropTypes.func,
+  createSpot: PropTypes.func,
   lot: PropTypes.object
 };
 
